69da4f04e6300e159bd598e9db4b5ab5e4cc3f74
Jechol Lee <mr.jechol@gmail.com>
2022-08-31 10:40:24 +0900

    Add formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,15 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.exs", "config/*.exs"],
+  subdirectories: ["apps/*"],
+  locals_without_parens: [
+    assert_map_match: 1,
+    setup_right: 1,
+    assert_right: 1,
+    assert_left_error: 1,
+    assert_just: 1,
+    assert_nothing: 1,
+    assert_map_match: 1,
+    refute_map_match: 1,
+    reather: 2
+  ]
 ]
